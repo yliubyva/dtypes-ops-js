7a4a1a4c83dd0a435a8915c34d62c9f7c7d54592
yliubyva <yliubyva@gmail.com>
2024-08-06 18:42:41 +0300

    variable assignment strategy has been changed

--- task-max.js
+++ task-max.js
@@ -1,11 +1,10 @@
 'use strict';
 
 //max-1
-let sumDeposit;
 let period = 2;
 let percentYear = 5;
 
-sumDeposit = prompt(`Яка сума вкладу в банк?`);
+let sumDeposit = prompt(`Яка сума вкладу в банк?`);
 let chargeAmount = sumDeposit * ((percentYear / 100) * (period / 12));
 alert(`Ваша сума нарахованих відсотків ${chargeAmount.toFixed(2)}`);
 
--- task-norm.js
+++ task-norm.js
@@ -1,20 +1,15 @@
 'use strict';
 
 //norm-1 
-let userMoney;
-let costChocolate;
-let quantityChocolate;
-let rest;
+let userMoney = prompt(`Скільки грошей у Вашому гаманці?`);
+let costChocolate = prompt(`Яка ціна шоколадки?`);
 
-userMoney = prompt(`Скільки грошей у Вашому гаманці?`);
-costChocolate = prompt(`Яка ціна шоколадки?`);
-
-quantityChocolate = userMoney / costChocolate;
+let quantityChocolate = userMoney / costChocolate;
 let quantity = Math.trunc(quantityChocolate);
 
 alert(`${quantity} шт шоколадок ти можеш купити`);
 
-rest = userMoney - quantity * costChocolate;
+let rest = userMoney - quantity * costChocolate;
 
 alert(`Твоя решта ${rest}`);
 
